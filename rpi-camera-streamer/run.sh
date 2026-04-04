f0b4049414a7b671ed0a1e3067c0e2ea946c201e
Brawndo <130789582+Brawndo-Corp@users.noreply.github.com>
2026-04-04 00:54:18 -0400

    Update run.sh

--- rpi-camera-streamer/run.sh
+++ rpi-camera-streamer/run.sh
@@ -8,46 +8,46 @@ PROFILE=$(bashio::config 'h264_profile')
 
 bashio::log.info "Brawndo Corp Streamer Starting..."
 
-echo "========================================"
-echo "TESTING LIBCAMERA HARDWARE ACCESS..."
-echo "========================================"
+#echo "========================================"
+#echo "TESTING LIBCAMERA HARDWARE ACCESS..."
+#echo "========================================"
 
 # Ask libcamera to list all physically connected sensors
-rpicam-hello --list-cameras
+#rpicam-hello --list-cameras
 
-echo "========================================"
-echo "TEST FINISHED. KEEPING CONTAINER ALIVE..."
-echo "========================================"
+#echo "========================================"
+#echo "TEST FINISHED. KEEPING CONTAINER ALIVE..."
+#echo "========================================"
 
 # Sleep for a few minutes so the Home Assistant Supervisor doesn't 
 # immediately see the script end and trigger a crash loop
-sleep 300
+#sleep 300
 # Start MediaMTX
-#/usr/local/bin/mediamtx /etc/mediamtx.yml &
-#sleep 2
+/usr/local/bin/mediamtx /etc/mediamtx.yml &
+sleep 2
 
-#WIDTH=$(echo $RES | cut -d'x' -f1)
-#HEIGHT=$(echo $RES | cut -d'x' -f2)
+WIDTH=$(echo $RES | cut -d'x' -f1)
+HEIGHT=$(echo $RES | cut -d'x' -f2)
 
 # Start rpicam-vid with IMX500 optimizations
-#rpicam-vid \
-#    -t 0 \
-#    --camera "${CAMERA_PATH}" \
-#    --width "$WIDTH" \
-#    --height "$HEIGHT" \
-#    --framerate "${FPS}" \
-#    --bitrate "${BITRATE}" \
-#    --profile "${PROFILE}" \
-#    --level 4.2 \
-#    --inline \
-#    --flush \
-#    --denoise cdn_off \
-#    --nopreview \
-#    -o - | ffmpeg \
-#        -hide_banner \
-#        -loglevel error \
-#        -i - \
-#        -c copy \
-#        -f rtsp \
-#        -rtsp_transport tcp \
-#        rtsp://localhost:8554/live
+rpicam-vid \
+    -t 0 \
+    --camera "${CAMERA_PATH}" \
+    --width "$WIDTH" \
+    --height "$HEIGHT" \
+    --framerate "${FPS}" \
+    --bitrate "${BITRATE}" \
+    --profile "${PROFILE}" \
+    --level 4.2 \
+    --inline \
+    --flush \
+    --denoise cdn_off \
+    --nopreview \
+    -o - | ffmpeg \
+        -hide_banner \
+        -loglevel error \
+        -i - \
+        -c copy \
+        -f rtsp \
+        -rtsp_transport tcp \
+        rtsp://localhost:8554/live
